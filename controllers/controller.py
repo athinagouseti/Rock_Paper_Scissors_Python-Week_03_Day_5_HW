3b071479f9220a958120e03c9e3d26b7cae5015e
Athina Gouseti <athina.gouseti@outlook.com>
2021-09-17 22:15:13 +0100

    Display user choices

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,6 +3,14 @@ from flask import render_template
 from models.player import Player
 from models.game import Game
 
-@app.route("/rock/scissors")
+@app.route("/task1/rock/scissors")
 def player_1_wins_with_rock():
     return "Player 1 wins by playing rock"
+
+@app.route("/<player_1_selection>/<player_2_selection>")
+def two_player_game_route_handler(player_1_selection, player_2_selection):
+    game = Game()
+    player_1 = Player("Player 1", player_1_selection)
+    player_2 = Player("Player 2", player_2_selection)
+    winner = game.determine_game(player_1, player_2) 
+    return f"Player 1 choice is {player_1_selection} and Player 2 choice is {player_2_selection}, the winner is {winner.name}"
